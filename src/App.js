7386d3b2bfcd984a7512c1679e7e773bc2f22ab9
Ryo Kotaki <ryooptimus@gmail.com>
2021-11-10 18:52:09 +0900

    implemnt callback with useEffect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React,{useState} from 'react';;
+import React,{useState,useEffect} from 'react';;
 
 function App(props) {
   const[state,setState] =useState(props)
   const{name,price} =state
-  const reset = ()=>{setState(props)}
+
+  useEffect(()=>{
+    console.log("useEffect is invoked")
+  })
+  useEffect(()=>{
+    console.log("componendDidMount ")
+  },[])
+  useEffect(()=>{
+    console.log("callback name ")
+  },[name])
+
   return (
     <>
       <p>現在の{name}は,{price}円です</p>
       <button onClick={()=> setState({...state,price: price+1})}>+1</button>
       <button onClick={()=> setState({...state,price: price-1})}>-1</button>
-      <button onClick={reset}>Reset</button>
+      <button onClick={()=>setState(props)}>Reset</button>
       <input value={name} onChange={e => setState({...state,name:e.target.value})}/>
     </>
   );
