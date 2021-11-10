c2626337ed3e5379260dd53af81269bc9dd82fcf
Ryo Kotaki <ryooptimus@gmail.com>
2021-11-10 18:18:36 +0900

    implement pbject state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React,{useState} from 'react';;
 
 function App(props) {
-  const[name,setName] =useState(props.name)
-  const[price,setPrice] = useState(props.price)
-  const reset = ()=>{
-    setPrice(props.price)
-    setName(props.name)
-  }
+  const[state,setState] =useState(props)
+  const{name,price} =state
+  const reset = ()=>{setState(props)}
   return (
     <>
       <p>現在の{name}は,{price}円です</p>
-      <button onClick={()=> setPrice(price+1)}>+1</button>
-      <button onClick={()=> setPrice(price-1)}>-1</button>
+      <button onClick={()=> setState({...state,price: price+1})}>+1</button>
+      <button onClick={()=> setState({...state,price: price-1})}>-1</button>
       <button onClick={reset}>Reset</button>
-      <input value={name} onChange={e => setName(e.target.value)}/>
+      <input value={name} onChange={e => setState({...state,name:e.target.value})}/>
     </>
   );
 }
